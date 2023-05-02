3bda7388588ebd343cf0cd5e8997db3f0833fc27
removedporn <86824510+removedporn@users.noreply.github.com>
2023-05-02 16:04:53 +0800

    Delete src directory

--- src/config.js
+++
@@ -1,2 +0,0 @@
-const BASE_URL = 'https://esepicmail.herokuapp.com/api/v1';
-export default BASE_URL;
--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
-import reducers from './reducers';
-
-const store = createStore(reducers, applyMiddleware(ReduxPromise));
-
-export default store;
